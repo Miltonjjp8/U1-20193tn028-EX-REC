366d915b74cd2f5f7832340acf3c2ac019b570e6
Miltonjjp8 <83998282+Miltonjjp8@users.noreply.github.com>
2023-07-04 20:57:13 -0600

    recupera
    recupera pipipi

---
+++ Recupera/carro.js
@@ -0,0 +1,38 @@
+var producto = {
+    nombre: 'plancha',
+    precioUnitario: 0,
+    cantidad: 0,
+
+    calcularTotal:()=>{
+        console.log("estoy hablando");
+    }
+};
+
+var carrito = {
+    productos: [],
+    folio: 0,
+    subTotal: 0,
+    total:0,
+
+    calcular:()=>{
+        console.log("estoy gritando");
+    }
+};  
+
+
+
+const btnAddTelevision = document.getElementById("agregarTele");
+const btnAddLavadora= document.getElementById("agregarLavadora");
+const btnAddPlancha= document.getElementById("agregarPlancha");
+
+btnAddTelevision.addEventListener("click", () => {
+    
+});
+
+btnAddLavadora.addEventListener("click", () => {
+    
+});
+
+btnAddPlancha.addEventListener("click", () => {
+    
+});
